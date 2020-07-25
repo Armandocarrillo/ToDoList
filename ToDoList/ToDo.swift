870d521fe6889895331f355f98a70e532dd28d7c
Armando Carrillo <ingcompuarmando@gmail.com>
2020-07-25 12:42:43 -0500

    Configure the table view controller

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Armando Carrillo on 25/07/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo{
+    var title: String
+    var isComplete: Bool
+    var notes: String?
+}
